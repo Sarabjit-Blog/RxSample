e61e0107ffdeac1d391dee673048e5b7d68acaa3
Sarabjit Bagga <>
2019-05-19 02:05:41 +0100

    First Commit

---
+++ app/src/main/java/blog/sarabjit/MainActivity.java
@@ -0,0 +1,52 @@
+package blog.sarabjit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "RxSample";
+    private static final String SAY_HELLO = "Hello From Sarab";
+    private Observable<String> myObservable;
+    private Observer<String> myObserver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        myObservable = Observable.just(SAY_HELLO);
+        myObservable.subscribeOn(AndroidSchedulers.mainThread());
+        myObserver = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "On Disposable");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "On Next");
+                Toast t = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG);
+                t.setGravity(Gravity.CENTER, 0, 0);
+                t.show();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "On Error");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "On Complete");
+            }
+        };
+        myObservable.subscribe(myObserver);
+    }
+}
